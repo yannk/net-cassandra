cbf6b56b5131bc73deb09313c5af40ab5f46a9c0
Yann Kerherve <yann@cyberion.net>
2010-04-25 08:51:52 -0700

    Oops dupe line

--- mangle-thrift.pl
+++ mangle-thrift.pl
@@ -6,7 +6,6 @@ my $interface = shift;
 system(thrift => '--gen' => perl => $interface)
     && die "Thrift generation error: $!";
 
-system "rm -r ./gen-perl";
 system "perl -p -i -e 's/Cassandra::/Net::Cassandra::Backend::/g; s/Thrift::TException/Net::Cassandra::Backend::Thrift::TException/g; s/use Thrift/use Net::Cassandra::Backend::Thrift/g' gen-perl/Cassandra/*.pm";
 system "cp gen-perl/Cassandra/*.pm lib/Net/Cassandra/";
 system "rm -r ./gen-perl";
